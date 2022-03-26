8bbc1f6d8609191963552cb8600ea75a33756829
Jackson Naufal <Jackson.naufal@mths.ca>
2022-03-26 07:58:17 +0000

    FixedErrors

--- triangle_perimeter_calculator.py
+++ triangle_perimeter_calculator.py
@@ -9,7 +9,8 @@ def main():
     # This is a calculator that calculates the area and perimeter of a rectangle
     # input
     print(
-        "This is a perimeter of a triangle calculator, please input your triangles values down below!"
+        "This is a perimeter of a triangle calculator, please input"
+        "your triangles values down below!"
     )
     sideA = int(input("\nEnter side A of rectangle (mm): "))
     sideB = int(input("Enter side B of rectangle (mm): "))
@@ -22,7 +23,7 @@ def main():
     print("\nYour equation, for your triangle is")
     print("\nP = a + b + c")
     print("P = {0} + {1} + {2}".format(sideA, sideB, sideC))
-    print("P = {0}".format(trianglePerimeter))
+    print("P = {0} mm".format(trianglePerimeter))
     print("\nThe perimeter of your triangle is {0} mm.".format(trianglePerimeter))
     print("\nDone.")
 
